43cfc41c25c2a4dfb9b1fc54bdd1cbee8a72a9f3
yannick DOLLOU <yannick.dollou@axa.fr>
2022-10-17 16:21:20 +0200

    -refactor

--- src/main/java/com/openclassrooms/safetynetsalertsprojects/model/DataSource.java
+++ src/main/java/com/openclassrooms/safetynetsalertsprojects/model/DataSource.java
@@ -9,55 +9,13 @@ import java.io.File;
 import java.io.IOException;
 import java.util.List;
 
-//@Component
-//public class DataSource {
-//    @Autowired
-//   private ObjectMapper mapper;
-//    String JSONPATH ="src/main/resources/json/jsondata";
-//
-//    private List<Persons> persons ;
-//    private List<FireStations> firestations;
-//    private List<MedicalRecords> medicalrecords;
-//
-//    public List<Persons> getPersons() {
-//        return persons;
-//    }
-//
-//    public void setPersons(List<Persons> persons) {
-//        this.persons = persons;
-//    }
-//
-//    public List<FireStations> getFirestations() {
-//        return firestations;
-//    }
-//
-//    public void setFirestations(List<FireStations> firestations) {
-//        this.firestations = firestations;
-//    }
-//
-//    public List<MedicalRecords> getMedicalrecords() {
-//        return medicalrecords;
-//    }
-//
-//    public void setMedicalrecords(List<MedicalRecords> medicalrecords) {
-//        this.medicalrecords = medicalrecords;
-//    }
-//
-//@PostConstruct
-//    public void init() throws IOException {
-//        DataSource dataSource = mapper.readValue(new File(JSONPATH), DataSource.class);
-//        this.persons = dataSource.getPersons();
-//        this.firestations = dataSource.getFirestations();
-//        this.medicalrecords = dataSource.getMedicalrecords();
-//    }
-//}
-
 @Component
 public class DataSource {
     @Autowired
    private ObjectMapper mapper;
+    String JSONPATH ="src/main/resources/json/jsondata";
 
-    private List<Persons> persons;
+    private List<Persons> persons ;
     private List<FireStations> firestations;
     private List<MedicalRecords> medicalrecords;
 
@@ -85,12 +43,14 @@ public class DataSource {
         this.medicalrecords = medicalrecords;
     }
 
-    @PostConstruct
-    public void init() throws IOException{
-        DataSource dataSource = mapper.readValue("src/main/resources/json/jsondata",DataSource.class);
-        this.persons= dataSource.getPersons();
-        this.firestations= dataSource.getFirestations();
+@PostConstruct
+    public void init() throws IOException {
+        DataSource dataSource = mapper.readValue(new File(JSONPATH), DataSource.class);
+        this.persons = dataSource.getPersons();
+        this.firestations = dataSource.getFirestations();
         this.medicalrecords = dataSource.getMedicalrecords();
     }
 }
+
+
 
